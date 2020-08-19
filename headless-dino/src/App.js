08814369e8b8a7806f8115467139feef04d719cc
Avery McCarthy <69693126+theaverymccarthy@users.noreply.github.com>
2020-08-19 17:33:45 -0400

    added dropdown menu and go button. Added comments

--- headless-dino/src/App.js
+++ headless-dino/src/App.js
@@ -3,12 +3,34 @@ import './App.css';
 /* to start webpage open command prompt 
   cd headless-dino
   npm start
+  wait a second
 */
 function App() {
   return (     
+    <>  
+      <title>Legendary Monster Generator</title> {/* title isn't changing for some reason*/}
       <body>
-        Hello Peter...
+        
+        Greetings. Welcome to the Headless Dino Legendary Monster Generator.
+        
+        <p>
+          Press the button to generate your Legendary Monster. Good luck to your adventurers.
+        </p>
+        <br></br>
+        <h1 style={{float: 'left'}}> {/*creating dropdown menu to select biome.Need to add style to it and add more. Maybe make into a table instead*/}
+          <label for="biomes">Choose a biome:</label>
+            <select name="biomes" id="biomes">
+              <option value="arid">Arid</option>
+              <option value="ocean">Ocean</option>
+              <option value="woodland">Woodland</option>
+              <option value="desert">Desert</option>
+            </select>
+        </h1>
+        <br></br>
+      {/* summon button <button>Summon Me</button> */}
+
       </body>
+    </>
   );
 }
 
